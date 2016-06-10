4d42ea7206204ff5dbf5eaa98f027c8bf3bb34c1
Júnior <w.carreirojunior@gmail.com>
2016-06-10 17:58:52 -0300

    Ensure separate partition exists for /tmp

--- pwnedless_RH7.sh
+++ pwnedless_RH7.sh
@@ -40,6 +40,21 @@ echo ""
 echo "#Ensure mounting of FAT filesystems is disabled" > $file_pwnedless
 echo "install vfat /bin/true" > $file_pwnedless
 
+sleep 2
+
+echo ">>>>Ensure separate partition exists for /tmp"
+mount | grep /tmp
+audit=`echo $?`
+    if $audit = 1
+    	then
+            systemctl unmask tmp.mount
+            systemctl enable tmp.mount
+        else
+            echo "The /tmp is partitioned"
+    fi
+
+sed 's/Options/#&/' /etc/systemd/system/local-fs.target.wants/tmp.mount
+sed '/Options/ a Options=mode=1777,strictatime,noexec,nodev,nosuid' /etc/systemd/system/local-fs.target.wants/tmp.mount
 
 echo "##############################################"
 echo "###	installing pre requirements	 ###"
